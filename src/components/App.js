da0acbc400dda6a27f17adf3a2643d5d47d0ce8b
Kumaresh Ghosh <74142329+KumareshGhosh@users.noreply.github.com>
2022-08-25 00:25:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -77,6 +77,31 @@ class App extends Component {
 
     this.setState({ loading: false });
   }
+ // staking tokens
+  stakeTokens = (amount) => {
+    this.setState({ loading: true });
+    this.state.tether.methods
+      .approve(this.state.decentralBank._address, amount)
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.state.decentralBank.methods
+          .depositTokens(amount)
+          .send({ from: this.state.account })
+          .on("transactionHash", (hash) => {
+            this.setState({ loading: false });
+          });
+      });
+  };
+  //unstaking function
+  unstakeTokens = () => {
+    this.setState({ loading: true });
+    this.state.decentralBank.methods
+      .unstakeTokens()
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.setState({ loading: false });
+      });
+  };
 
   constructor(props) {
     super(props);
